cedf574ddf439baf0b5d685444729a10ac2ddc52
AniaSin <pierrat.laurent@gmail.com>
2022-10-30 14:13:29 +0100

    Split text but not words Score

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
+from textwrap import wrap
 
-TEXT = "Je me suis rendu compte peu à peu de ce que fut jusqu'à présent toute grande philosophie : la confession de son " \
+TEXT_ = "Je me suis rendu compte peu à peu de ce que fut jusqu'à présent toute grande philosophie : la confession de son " \
        "auteur, une sorte de mémoires involontaires et insensibles ; et je me suis aperçu aussi que les intentions " \
        "morales ou immorales formaient, dans toute philosophie, le véritable germe vital d'où chaque fois la plante " \
        "entière est éclose. On ferait bien en effet (et ce serait même raisonnable) de se demander, pour l'élucidation " \
@@ -24,10 +25,12 @@ TEXT = "Je me suis rendu compte peu à peu de ce que fut jusqu'à présent toute
        "d'impersonnel ; et particulièrement sa morale témoigne, d'une façon décisive et absolue, de ce qu'il est, " \
        "c'est-à-dire dans quel rapport se trouvent les instincts les plus intimes de sa nature."
 
-TEXT.replace(" – ", " ")
-split_text = [TEXT[idx:idx + 60] for idx in range(0, len(TEXT), 60)]
+
+TEXT = TEXT_.replace(" – ", " ")
+split_text = wrap(TEXT, 60)
 TIME_TEST = 300
 
+cursor_pos = 0
 count = TIME_TEST
 timer = ""
 
@@ -47,6 +50,12 @@ def start(time_left):
 def check_typing():
     if count < TIME_TEST:
         current_time = TIME_TEST - count
+        current_words_count = 0
+        for text in texts_down[:focus_index + 1]:
+            current_words_count += len(text.get("1.0", "end").split())
+        words_min = current_words_count / current_time
+        words_min = "{:.2f}".format(words_min * 60)
+        label_result.configure(text=f"{words_min} words/minutes")
 
 
 def game_end():
@@ -55,10 +64,13 @@ def game_end():
 
 def key_press(event):
     global focus_index
+    global cursor_pos
     shift_keys = {"Shift_L", "Shift_R"}
     if event.keysym not in shift_keys:
         index = int(texts_down[focus_index].index(tk.INSERT).split(".")[1]) - 1
         print(index)
+        print(focus_index)
+        cursor_pos = index
         try:
             if event.char == texts_up[focus_index].get(texts_up[focus_index].index(f"1.{index}")):
                 texts_up[focus_index].tag_add("correct", texts_up[focus_index].index(f"1.{index}"))
@@ -77,10 +89,14 @@ def key_press(event):
 
 def do_backspace(event):
     global focus_index
+    global cursor_pos
     shift_keys = {"Shift_L", "Shift_R"}
     if event.keysym not in shift_keys:
         try:
             index = int(texts_down[focus_index].index(tk.INSERT).split(".")[1])
+            cursor_pos = index
+            if focus_index == 0:
+                return
             if index == 0:
                 focus_index -= 1
                 for text_box in texts_up:
@@ -88,7 +104,9 @@ def do_backspace(event):
                 for text_box in texts_down:
                     text_box.place(y=text_box.winfo_y() + 80)
                 index = int(texts_down[focus_index].index(tk.INSERT).split(".")[1]) - 1
+                cursor_pos = index
                 texts_down[focus_index].focus()
+                texts_down[focus_index].delete("end-2c")
             texts_up[focus_index].tag_remove("wrong", texts_up[focus_index].index(f"1.{index}"))
             texts_up[focus_index].tag_remove("correct", texts_up[focus_index].index(f"1.{index}"))
         except tk.TclError:
@@ -117,6 +135,7 @@ texts_up = []
 texts_down = []
 for line in split_text:
     current_index = split_text.index(line)
+    line += " "
     texts_up.append(tk.Text(width=50, height=1, pady=2,  relief="flat", fg="grey", font=("Arial", 14, "normal")))
     texts_up[current_index].insert(f"{current_index}.0", line)
     texts_up[current_index].config(state="disabled")
@@ -131,4 +150,5 @@ for line in split_text:
 focus_index = 0
 texts_down[focus_index].focus()
 
+start(TIME_TEST)
 win.mainloop()
